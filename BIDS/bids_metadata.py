c98e61d756eb9ec79cddc4ad906978ecb08a2828
Hanane BOUDLAL <hanaboud8@gmail.com>
2026-06-03 17:52:05 +0200

    fixing slice index mapping for the 3 main scripts -> same logic for all

--- BIDS/bids_metadata.py
+++ BIDS/bids_metadata.py
@@ -477,7 +477,8 @@ def map_old_index_to_reoriented_index(
         new_index.append(value)
 
     return tuple(new_index)
-def build_2d_sform_for_volume(
+
+def build_2d_sform(
     width: float,
     height: float,
     pixel_size: list[float],
@@ -605,17 +606,11 @@ def add_sform_to_json_metadata(
     nb_slices = len(slice_position_map)
     width_phys = meta.get("WidthPhysical")
     height_phys = meta.get("HeightPhysical")
-    pixel_size = meta["PixelSize"]
 
-    if width_phys is not None and height_phys is not None:
-        virtual_width  = width_phys  / float(pixel_size[0])
-        virtual_height = height_phys / float(pixel_size[1])
-    else:
-        virtual_width  = round(meta["Width"])
-        virtual_height = round(meta["Height"])
-    sform = build_2d_sform_for_volume(
-        width=virtual_width,
-        height=virtual_height,
+    
+    sform = build_2d_sform(
+        width=width_phys,
+        height=height_phys,
         pixel_size=meta["PixelSize"],
         slice_position=slice_position,
         nb_slices=nb_slices,
