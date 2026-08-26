fe21664080c2b56f61c7a279f26a9fcd72e45b7e
hanane-boudlal <hanaboud8@gmail.com>
2026-08-26 18:25:52 +0200

    fixing some stuffs

--- python_scripts/slice_finder.py
+++ python_scripts/slice_finder.py
@@ -10,8 +10,7 @@ N = int(meta0["NumberOfSlices"])
 thickness = max(np.linalg.norm(nb.load(files[0][:-5] + ".nii.gz").affine[:3, :3], axis=0))
  
 # Z (mm) -> SlicePosition :  k = z / thickness + (N-1)/2
-k = round(-(z / thickness + (N - 1) / 2))
- 
+k = round((N - 1) / 2 - z / thickness) 
 for f in files:
     meta = json.load(open(f))
     if meta.get("SlicePosition") == k:
